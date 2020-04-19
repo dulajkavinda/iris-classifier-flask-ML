5eb790ee7778cd5d475fad341b9d93cfacd133b2
Himantha Tharindu <IT17019446@my.sliit.lk>
2020-04-19 07:03:22 +0530

    changin the frontpage

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Welcome to Iris Prediction server !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
